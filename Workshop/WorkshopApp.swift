f24fa30098c6837b9528e57cc85972d7d2fcfc7e
dhustnjak <domagoj.hustnjak@infinum.com>
2023-10-26 22:56:15 +0200

    Create project

---
+++ Workshop/WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopApp.swift
+//  Workshop
+//
+//  Created by Domagoj Hustnjak on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+
+        }
+    }
+}
